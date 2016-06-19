be7a778ac7caf4d546f75cf9fda8d17d7a578935
karin <summertriangle.dev@gmail.com>
2016-06-19 16:22:04 -0700

    update to 1.9.9

--- versioncheck.py
+++ versioncheck.py
@@ -75,7 +75,7 @@ class ApiClient(object):
             "USER_ID": self.lolfuscate(str(self.user)),
             "CARRIER": "google",
             "UDID": self.lolfuscate(self.udid),
-            "APP_VER": "1.9.4",
+            "APP_VER": "1.9.9",
             "RES_VER": str(self.res_ver),
             "IP_ADDRESS": "127.0.0.1",
             "DEVICE_NAME": "Nexus 42",
@@ -118,4 +118,3 @@ if __name__ == '__main__':
         print(sys.argv[1])
     else:
         print(versioncheck().decode("ascii"))
-
